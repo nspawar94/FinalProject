0787246e0bddb57f1af9db1b6f1d1273cbb5b188
wariyat <wariya.tang@gmail.com>
2018-12-01 18:18:11 -0500

    Main Activity Create Account Request Confirmation

--- app/src/main/java/com/example/nikhil/finalproject/MainActivity.java
+++ app/src/main/java/com/example/nikhil/finalproject/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.nikhil.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button buttonSignIn,buttonSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+        buttonSignIn.setOnClickListener(this);
+        buttonSignUp.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonSignUp){
+            Intent intentSignUp = new Intent(this,CreateAccount.class);
+            startActivity(intentSignUp);
+    }}
 }
