f6424bc0fca5620a7b74da5d50fb7d2b5572a11d
gitjobb <attha@umich.edu>
2018-12-02 20:33:25 -0500

    Update Onclick Listener for donor profile & term of use

--- app/src/main/java/com/example/nikhil/finalproject/TermsOfUse.java
+++ app/src/main/java/com/example/nikhil/finalproject/TermsOfUse.java
@@ -2,12 +2,30 @@ package com.example.nikhil.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TermsOfUse extends Activity {
+public class TermsOfUse extends Activity implements View.OnClickListener{
+
+    Button buttonDisagree, buttonAgree;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_of_use);
+
+        buttonDisagree = findViewById(R.id.buttonDisagree);
+        buttonAgree = findViewById(R.id.buttonAgree);
+
+        buttonDisagree.setOnClickListener(this);
+        buttonAgree.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
