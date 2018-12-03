c8685dda0472d932ac3d75c7e53e19c9ce05d6cb
Nikhil <nspawar@umich.edu>
2018-12-02 21:56:04 -0500

    setup1

--- app/src/main/java/com/example/nikhil/finalproject/RequestConfirmPage.java
+++ app/src/main/java/com/example/nikhil/finalproject/RequestConfirmPage.java
@@ -9,5 +9,6 @@ public class RequestConfirmPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_confirm_page);
+
     }
 }
--- app/src/main/java/com/example/nikhil/finalproject/RequestDashboard.java
+++ app/src/main/java/com/example/nikhil/finalproject/RequestDashboard.java
@@ -2,12 +2,31 @@ package com.example.nikhil.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class RequestDashboard extends Activity {
+public class RequestDashboard extends Activity implements View.OnClickListener {
+    Button buttonAccept1, buttonAccept2, buttonCant1, buttonCant2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_dashboard);
+
+        buttonAccept1 = findViewById(R.id.buttonAccept1);
+        buttonAccept2 = findViewById(R.id.buttonAccept2);
+        buttonCant1 = findViewById(R.id.buttonCant1);
+        buttonCant2 = findViewById(R.id.buttonCant2);
+
+        buttonAccept1.setOnClickListener(this);
+        buttonAccept2.setOnClickListener(this);
+        buttonCant1.setOnClickListener(this);
+        buttonCant2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
