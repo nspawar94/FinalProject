88972e1e75fe44da4e3aca0ab0778253f8c7c9bd
gitjobb <attha@umich.edu>
2018-12-02 21:02:59 -0500

    Update Onclick Listener for close request activity

--- app/src/main/java/com/example/nikhil/finalproject/CloseRequestPage.java
+++ app/src/main/java/com/example/nikhil/finalproject/CloseRequestPage.java
@@ -2,12 +2,28 @@ package com.example.nikhil.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class CloseRequestPage extends Activity {
+public class CloseRequestPage extends Activity implements View.OnClickListener{
+
+    Button buttonBackToRequest, buttonConfirmClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_close_request_page);
+
+        buttonBackToRequest = findViewById(R.id.buttonBackToRequest);
+        buttonConfirmClose = findViewById(R.id.buttonConfirmClose);
+
+        buttonBackToRequest.setOnClickListener(this);
+        buttonConfirmClose.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
