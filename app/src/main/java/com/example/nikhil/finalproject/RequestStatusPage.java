5b66c3a3b46272f3831e362dbd19de2985ff1e89
gitjobb <attha@umich.edu>
2018-12-02 21:27:41 -0500

    Update request status page

--- app/src/main/java/com/example/nikhil/finalproject/RequestStatusPage.java
+++ app/src/main/java/com/example/nikhil/finalproject/RequestStatusPage.java
@@ -2,12 +2,29 @@ package com.example.nikhil.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class RequestStatusPage extends Activity {
+public class RequestStatusPage extends Activity implements View.OnClickListener {
+
+    Button buttonRefresh, buttonCloseRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_status_page);
+
+        buttonRefresh = findViewById(R.id.buttonRefresh);
+        buttonCloseRequest = findViewById(R.id.buttonCloseRequest);
+        
+        buttonRefresh.setOnClickListener(this);
+        buttonCloseRequest.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
