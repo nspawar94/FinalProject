2c305a9f44f54a28c94452a10fb0b7a02873dbc3
Maruay9876 <maruay@umich.edu>
2018-12-11 13:19:25 -0500

    create account

--- app/src/main/java/com/example/nikhil/finalproject/CreateAccount.java
+++ app/src/main/java/com/example/nikhil/finalproject/CreateAccount.java
@@ -24,10 +24,8 @@ public class CreateAccount extends Activity implements View.OnClickListener{
     Button buttonNext;
     EditText editTextName, editTextLastName, editTextAge, editTextEMail, editTextPassword,editTextConfirmPassword;
     Spinner spinnerGender, spinnerBloodType;
-    String [] gender, bloodType;
     String genderSelected, bloodTypeSelected;
     private FirebaseAuth mAuth;
-// this is a comment, it is very good
 
 
     @Override
@@ -45,7 +43,7 @@ public class CreateAccount extends Activity implements View.OnClickListener{
 
         spinnerGender = (Spinner) findViewById(R.id.spinnerGender);
         ArrayAdapter<String> genderAdapter = new ArrayAdapter<String>(CreateAccount.this,
-                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.gender));
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.genders));
         genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerGender.setAdapter(genderAdapter);
 
