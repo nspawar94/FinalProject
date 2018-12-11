8491d00cb5e45198ae7788ab387f02bf6b7e6802
Maruay9876 <maruay@umich.edu>
2018-12-10 21:18:14 -0500

    request page blah blah

--- app/src/main/java/com/example/nikhil/finalproject/CreateAccount.java
+++ app/src/main/java/com/example/nikhil/finalproject/CreateAccount.java
@@ -4,16 +4,49 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CreateAccount extends Activity implements View.OnClickListener {
     Button buttonNext;
+    EditText editTextName, editTextLastName, editTextAge, editTextEMail, editTextPassword,editTextConfirmPassword;
+    Spinner spinnerGender, spinnerBloodType;
+    String [] gender, bloodType;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextAge = findViewById(R.id.editTextAge);
+        editTextEMail = findViewById(R.id.editTextEMail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+
+        spinnerGender = findViewById(R.id.spinnerGender);
+        gender = new String[]{"male", "female", "other"};
+        spinnerBloodType = findViewById(R.id.spinnerBloodType);
+        bloodType = new String[]{"A-","A+","B-","B+","AB-","AB+","O-","O+"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, gender);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+
+
+
+
+
+
+
         buttonNext = findViewById(R.id.buttonNext);
         buttonNext.setOnClickListener(this);
+
 
     }
 
